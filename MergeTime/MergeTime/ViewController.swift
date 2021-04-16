09b70aa9fd60a14a8ced86a147a1cb4cd1c3f0af
Vladyslav Shkodych <vladyslav.shkodych@jmind.systems>
2021-04-16 17:23:24 +0300

    [add] new project

---
+++ MergeTime/MergeTime/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MergeTime
+//
+//  Created by Vlad Shkodich on 16.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
