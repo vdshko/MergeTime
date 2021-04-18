f37c3f4aeb79336e68ad29105c1f85f68310de12
Vladyslav Shkodych <vladyslav.shkodych@jmind.systems>
2021-04-18 03:41:53 +0300

    [update] application flow

---
+++ MergeTime/Module/Supporting Files/Module.h
@@ -0,0 +1,16 @@
+//
+//  Module.h
+//  Module
+//
+//  Created by Vlad Shkodich on 18.04.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Module.
+FOUNDATION_EXPORT double ModuleVersionNumber;
+
+//! Project version string for Module.
+FOUNDATION_EXPORT const unsigned char ModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Module/PublicHeader.h>
